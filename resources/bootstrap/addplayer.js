ddba7ab5df467221af847e743871823d263be17c
bibhav777 <bibhav.sapkotaqa@gmail.com>
2019-06-01 19:26:15 +0545

    players api

---
+++ resources/bootstrap/addplayer.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+       $('#addplayer').submit(function(event){
+          event.preventDefault();
+          var playerData= {
+            image: $('#image')[0].files[0],
+            fullname: $('#fullname').val(),
+            dob : $('#dob').val(),
+            address: $('#address').val(),
+            sports: $('#sports').val(),
+            height: $('#height').val(),
+            regisdate: $('#regisdate').val(),
+            position: $('#position').val()
+          }
+          var formdata= new FormData();
+           for(key in playerData){
+           formdata.append(key, playerData[key]);
+         }
+           
+  $.ajax({
+
+  url: 'http://localhost:3001/addplayers',
+  method: 'POST',
+  contentType: false,
+  processData:false,
+  data: formdata,
+  dataType: 'json',
+
+
+
+  success :function(result,status){
+      console.log(result);
+      console.log(status);  
+
+      //$('#successmessage').html(result.message);    
+  },
+
+  error:function(jqXHR,status) {
+    //console.log(jqXHR.responseJSON.message);
+    //$('#successmessage').html(jqXHR.responseJSON.message);
+
+  }
+ })
+})
+
+
+
+
+
+})
+	
